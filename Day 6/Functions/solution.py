5aeb560ade9d9f6380fe449c376ad330a2d0f647
TaavishThaman <taavishthaman97@gmail.com>
2024-08-30 20:09:16 +0530

    Day 6

--- Day 6/Functions/solution.py
+++ Day 6/Functions/solution.py
@@ -4,3 +4,21 @@ def my_function():
 
 
 my_function()
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+while not at_goal():
+    while front_is_clear():
+        move()
+    if at_goal():
+        break
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
---
+++ Day 6/Functions/task.py
@@ -0,0 +1,5 @@
+def my_func():
+    print("Hello")
+    print("Bye")
+
+my_func()
